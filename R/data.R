47a764938f2748609c99bfd67deed1d359c6c187
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-02-14 12:32:35 +0000

    Add clustering datasets

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Morse Code Data
+#'
+#' A dissimilarity matrix of rates of confusion between Morse code characters.
+#' @format A data frame with 36 rows and 36 columns, where
+#' each element is the proportion of times that the row character was confused
+#' with the column character by people unfamiliar with Morse code.
+#' @source morse.code.csv
+"data_tidy_morse"
+
+#' Distances between South African cities
+#'
+#' Proximity matrix of distances (in km) between South African cities.
+#'
+#' @format A data frame with 12 rows and 12 columns.
+#' @source SAdistances.xlsx
+"data_tidy_sa_distance"
+
+#' Food data
+#'
+#'
